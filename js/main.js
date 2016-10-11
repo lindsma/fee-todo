f4dc8fabf9b3b8cbf089f3897c2f50b390217c82
Lindsey Adams <linzma87@gmail.com>
2016-10-11 15:41:20 -0400

    Complete

--- js/main.js
+++ js/main.js
@@ -1,113 +1,106 @@
-// using:  objects, arrays, iterations, functions, modules,  and the DOM (using jquery to manipulate it)
-
-// Clicking on a todo item's text allows the user to edit the item inline (see image #2)
-
-// (If you want, you could add a confirmation popup to this action using confirm(...))
-
-// The incomplete item count should always be displayed in the bottom left corner (see mockup)
-
-// Clicking on "Active" at the bottom nav results in only showing incomplete items
-
-// Clicking on "Completed" at the bottom results in only showing complete items
-
-// Clicking on "All" at the bottom results in showing all items, regardless of status
-
-// Clicking on the circle to the left of an item toggles whether it is complete or not
-
-// Clicking "Clear completed" removes any todo item that is marked complete (delete from the UI)
-
 $(document).ready(function() {
 
     var todoObject = {
+        todosAll: [],
 
         // create HTML elements
-
         createElements: function(itemInput) {
-
             var listItem = $('<li>').attr('class', itemInput);
-            var article = $('<article>').attr('class', 'article').appendTo(listItem);
+            var article = $('<article>').attr('class', 'article incomplete').appendTo(listItem);
             var checkBtn = $('<button>').attr('class', 'check').appendTo(article);
-            var todoInput = $('<p>').attr('class', itemInput).html(itemInput).appendTo(article);
+            var todoInput = $('<p>').attr('class', 'incomplete ' + itemInput).text(itemInput).appendTo(article);
             var editTodo = $('<input>').attr({
-                'type': 'edit-todo',
-                // 'value': itemInput
+                'class': 'edit-todo',
+                'type': 'text',
+                'value': itemInput
             }).appendTo(article);
-            var closeBtn = $('<button>').attr('class', 'delete').html('X').appendTo(article);
-
+            var closeBtn = $('<button>').attr({
+                'class': 'delete',
+                'id': itemInput
+            }).html('X').appendTo(article);
             $(listItem).appendTo('.items');
-
-            this.handleEvents();
-
+            todoObject.updateFooter();
         },
-
         // get input from form
-
         getInput: function() {
-
-            var todoArray = [];
-
             $('form').submit(function(event) {
-
                 event.preventDefault();
                 var itemInput = $('.new-todo').val();
                 $('.new-todo').val('');
-
-                todoArray.push(itemInput);
+                todoObject.todosAll.push(itemInput);
                 todoObject.createElements(itemInput);
-
             });
-
         },
-
-        // declare event handlers
-
-        handleEvents: function() {
-
+        // initiate event handlers and call getInput
+        init: function() {
             // delete todo item
-
-            $('.items').on('click', '.delete', function() {
-
-                $(this).parents('.article').slideUp(function() {
-
+            $('.items').on('click', '.delete', function(event) {
+                var selectedInput = $(this).attr('id');
+                $(this).parents('.completed').slideUp(function() {
                     $(this).remove();
-
+                    var deleteInput = todoObject.todosAll.indexOf(selectedInput);
+                    todoObject.todosAll.splice(deleteInput, 1);
                 });
-
+                todoObject.updateFooter();
             });
-
             // toggle complete button
-
-            $('.items').on('click', '.check', function() {
-
-                $(this).parents('.article').slideUp(function() {
-
-                    $(this).remove();
-
-                });
-
+            $('.items').on('click', '.check', function(event) {
+                var selectedInput = $(this).siblings('p').html();
+                $(this).toggleClass('complete').html('&#10003');
+                var listP = $(this).siblings('p');
+                $(listP).toggleClass('complete').toggleClass('incomplete');
+                $(listP).parents('article').toggleClass('completed').toggleClass('incomplete');
+                $(listP).siblings('p').toggleClass('complete');
+                todoObject.updateFooter();
             });
+            // 'show-all' button
+            $('.show-all').click(function() {
+                $('article.completed').show();
+                $('article.incomplete').show();
+            });
+            // 'show active' button
+            $('.show-active').click(function() {
+                $('article.completed').hide();
+                $('article.incomplete').show();
+            });
+            // 'show completed' button
+            $('.show-completed').click(function() {
+                $('article.incomplete').hide();
+                $('article.completed').show();
+            });
+            // 'clear completed' button
+            $('.clear').click(function() {
+                $('.completed').remove();
+            });
+            // edit todo item
+            $('.items').on('click', 'p', function() {
+                var self = this;
+                $(self).hide();
+                var selectedInput = $(this).text();
+                $(this).siblings('.edit-todo').show();
+                $(this).parents('article').toggleClass('editing');
+                $('p.' + selectedInput).focus();
+                $(this).siblings('.delete').hide();
 
-
-
-        }
-
-
-        //     editInput: function() {
-        //
-        //     $('ul').on('click', '.check', function() {
-        //
-        //       console.log('hi');
-        //
-        //
-        //     });
-        //
-        //
-        //
-        // }
-
-
+                $('.edit-todo').keyup(function(event) {
+                    var newValue = this.value;
+                    $(self).html(newValue);
+                    if (event.keyCode == 13) {
+                        $(self).show();
+                        $(self).siblings('.edit-todo').hide();
+                        $(self).siblings('.delete').show();
+                    }
+                });
+            });
+            todoObject.getInput();
+        },
+        // update values for 'items left'
+        updateFooter: function() {
+            var incompleteItems = $('article.incomplete').length;
+            console.log(incompleteItems);
+            $('.incomplete-items').text('');
+            $('.incomplete-items').text(incompleteItems);
+        },
     };
-
-    todoObject.getInput();
-
+    todoObject.init();
 });
